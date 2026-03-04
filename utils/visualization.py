acf9ec5385c2d33c401e27b7f67f718e37c910e9
Karthick S <skarthickskarthick08921_bcs26@mepcoeng.ac.in>
2026-03-04 09:26:49 +0530

    Create visualization utilities for plotting and visualization functions.

---
+++ utils/visualization.py
@@ -0,0 +1,17 @@
+# Visualization Utilities
+
+This module provides functions for plotting and visualizing data relevant to AQI predictions.
+
+## Functions
+
+### plot_aqi_time_series(data)
+- Plots the time series of AQI values.
+
+### plot_aqi_distribution(data)
+- Visualizes the distribution of AQI values.
+
+### plot_aqi_comparison(data1, data2)
+- Compares AQI values from two different data sources.
+
+### save_plot(fig, filename)
+- Saves the generated plot to a file.
